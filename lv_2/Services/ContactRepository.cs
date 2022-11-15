713bcfb81fb5d5b3f2f8f910895a419218e62829
Iva Hodic <ivahodic@gmail.com>
2022-11-15 18:17:58 +0100

    Zadatak 2

--- lv_2/Services/ContactRepository.cs
+++ lv_2/Services/ContactRepository.cs
@@ -6,19 +6,69 @@ namespace lv_2.Services
     {
         public Contact[] GetAllContacts()
         {
-            return new Contact[]
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
             {
-            new Contact
-            {
-                Id = 1,
-                Name = "Glenn Block"
-            },
-            new Contact
-            {
-                Id = 2,
-                Name = "Dan Roth"
+                return (Contact[])ctx.Cache[CacheKey];
             }
+
+            return new Contact[]
+                {
+            new Contact
+            {
+                Id = 0,
+                Name = "Placeholder"
+            }
+                };
+        }
+        public ContactRepository()
+{
+    var ctx = HttpContext.Current;
+
+    if (ctx != null)
+    {
+        if (ctx.Cache[CacheKey] == null)
+        {
+            var contacts = new Contact[]
+            {
+                new Contact
+                {
+                    Id = 1, Name = "Glenn Block"
+                },
+                new Contact
+                {
+                    Id = 2, Name = "Dan Roth"
+                }
             };
+
+            ctx.Cache[CacheKey] = contacts;
+        }
+    }
+}
+
+        public bool SaveContact(Contact contact)
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                try
+                {
+                    var currentData = ((Contact[])ctx.Cache[CacheKey]).ToList();
+                    currentData.Add(contact);
+                    ctx.Cache[CacheKey] = currentData.ToArray();
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+
+            return false;
         }
     }
 }
