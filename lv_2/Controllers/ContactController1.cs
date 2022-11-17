ed888cbecdd938e0d522cb1f36114f7893a31c0b
Iva Hodic <ivahodic@gmail.com>
2022-11-17 18:40:10 +0100

    zadatak 3

--- lv_2/Controllers/ContactController1.cs
+++ lv_2/Controllers/ContactController1.cs
@@ -16,5 +16,13 @@ namespace lv_2.Controllers
             return contactRepository.GetAllContacts();
 
         }
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
     }
 }
